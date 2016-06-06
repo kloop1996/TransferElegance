f7365e6154ee6db879c659a080f50b4f10b1cb07
kloop1996 <kloop1996@gmail.com>
2016-06-06 10:36:29 +0300

    init

---
+++ app/src/androidTest/java/com/hmarka/kloop1996/transferelegance/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.hmarka.kloop1996.transferelegance;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
