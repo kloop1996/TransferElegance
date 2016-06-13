debdfa39099e6b7c1973cf9e96fca1ec1cb03fcd
kloop1996 <kloop1996@gmail.com>
2016-06-14 01:24:26 +0300

    add map fragment and fix

--- app/src/main/java/com/hmarka/kloop1996/transferelegance/ui/fragment/BookingFragment.java
+++ app/src/main/java/com/hmarka/kloop1996/transferelegance/ui/fragment/BookingFragment.java
@@ -16,11 +16,15 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.PlaceDetectionApi;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -37,7 +41,7 @@ import com.hmarka.kloop1996.transferelegance.viewmodel.BookingViewModel;
 /**
  * Created by kloop1996 on 11.06.2016.
  */
-public class BookingFragment extends Fragment implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener,GoogleMap.OnMyLocationButtonClickListener,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class BookingFragment extends Fragment implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener,PlaceSelectionListener {
     private BookingFragmentBinding bookingFragmentBinding;
     private GoogleApiClient mGoogleApiClient;
     private Location mLastLocation;
@@ -62,42 +66,33 @@ public class BookingFragment extends Fragment implements OnMapReadyCallback,Goog
         fragmentTransaction.add(R.id.map_container, mMapFragment);
         fragmentTransaction.commit();
 
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .addApi(Places.GEO_DATA_API)
-                    .addApi(Places.PLACE_DETECTION_API)
-                    .build();
-        }
+//        if (mGoogleApiClient == null) {
+//            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+//                    .addConnectionCallbacks(this)
+//                    .addOnConnectionFailedListener(this)
+//                    .addApi(LocationServices.API)
+//                    .addApi(Places.GEO_DATA_API)
+//                    .addApi(Places.PLACE_DETECTION_API)
+//                    .build();
+//        }
 
-        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
-                .getCurrentPlace(mGoogleApiClient, null);
-        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
-            @Override
-            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
-                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                    
-                }
-                likelyPlaces.release();
-            }
-        });
+//        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+//                .getCurrentPlace(mGoogleApiClient, null);
+//        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+//            @Override
+//            public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+//                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+//
+//                }
+//                likelyPlaces.release();
+//            }
+//        });
+
+        initAutocompleteView();
+
         return bookingFragmentBinding.getRoot();
 
     }
-
-    @Override
-    public void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    public void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -110,39 +105,42 @@ public class BookingFragment extends Fragment implements OnMapReadyCallback,Goog
 
     @Override
     public void onMapLongClick(LatLng latLng) {
-        bookingFragmentBinding.currentLocation.setText(latLng.latitude+" "+latLng.longitude);
 
+
+
+    }
+
+
+    @Override
+    public void onPlaceSelected(Place place) {
+       MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(place.getLatLng());
+        markerOptions.title(place.getAddress().toString());
+
+
+        mGoogleMap.addMarker(markerOptions);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
     }
 
     @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-
-        if (mLastLocation != null) {
-
-            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()),
-                    13));
-
-            mGoogleMap.addMarker(new MarkerOptions().title("Памятник коту")
-                    .snippet("Памятник коту Елизару")
-                    .position(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude())));
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
+    public void onError(Status status) {
 
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    private void initAutocompleteView(){
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment_from);
+        autocompleteFragment.setHint(getResources().getString(R.string.from_hint));
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+
+        autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment_to);
+        autocompleteFragment.setHint(getResources().getString(R.string.destination_hint));
+        autocompleteFragment.setOnPlaceSelectedListener(this);
     }
+
 }
