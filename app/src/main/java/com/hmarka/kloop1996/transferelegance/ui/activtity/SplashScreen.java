1b77f5ffc4df75f8e3b56305126cdf85a045e201
kloop1996 <kloop1996@gmail.com>
2016-06-15 21:24:57 +0300

    merge with alex

--- app/src/main/java/com/hmarka/kloop1996/transferelegance/ui/activtity/SplashScreen.java
+++ app/src/main/java/com/hmarka/kloop1996/transferelegance/ui/activtity/SplashScreen.java
@@ -9,40 +9,37 @@ import com.hmarka.kloop1996.transferelegance.R;
 import com.hmarka.kloop1996.transferelegance.TransferEleganceApplication;
 
 /**
- * Created by kloop1996 on 09.06.2016.
+ * Created by Alexey on 15.06.2016.
  */
-public class ActivityLauncher extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.splash_screen);
 
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
+//                Intent mainIntent = new Intent(SplashScreen.this, ActivityLauncher.class);
+
                 Intent mainIntent;
-                TransferEleganceApplication transferEleganceApplication = TransferEleganceApplication.get(ActivityLauncher.this);
+                TransferEleganceApplication transferEleganceApplication = TransferEleganceApplication.get(SplashScreen.this);
 
                 if (transferEleganceApplication.isUserLoad()) {
-                    mainIntent =  new Intent(ActivityLauncher.this, MainActivity.class);
+                    mainIntent =  new Intent(SplashScreen.this, MainActivity.class);
                 } else {
-                    mainIntent = new Intent(ActivityLauncher.this, LoginActivity.class);
+                    mainIntent = new Intent(SplashScreen.this, LoginActivity.class);
                 }
 
-                ActivityLauncher.this.startActivity(mainIntent);
-                ActivityLauncher.this.finish();
+//                ActivityLauncher.this.startActivity(mainIntent);
+//                ActivityLauncher.this.finish();
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
-
-
-
-
-        finish();
-
-
     }
 }
